f40cd912fa990c28a826111ea86f59410e29c07d
dcc635 <dan@cleary.io>
2014-10-17 10:07:22 -0700

    Completed Lesson 12 - Django Context

--- src/contact/views.py
+++ src/contact/views.py
@@ -1,15 +1,42 @@
+from django.conf import settings
+from django.core.mail import send_mail
 from django.shortcuts import render
 
 from .forms import ContactForm
 
 
 def home(request):
+    title = "Contact Us"
     form = ContactForm(request.POST or None)
+    confirm_message = None
 
     if form.is_valid():
-        print(form.cleaned_data['email'])
-        print(request.POST)
+        comment = form.cleaned_data['comment']
+        name = form.cleaned_data['name']
+        email_subject = 'Message to Seven.com'
+        email_message = '{comment} {name}'.format(**locals())
+        email_from = form.cleaned_data['email']
+        email_to = [settings.EMAIL_HOST_USER]
+        print(email_subject, email_message, email_from, email_to)
+        # store the message
+        send_mail(
+            email_subject,
+            email_message,
+            email_from,
+            email_to,
+            fail_silently=True,
+        )
+        title = 'Thank you'
+        form = None
+        confirm_message = """
+Thank you for your message. We have received it and we are reviewing it.
+"""
 
-    context = locals()
+    context = {
+        'title': title,
+        'form': form,
+        'confirm_message': confirm_message,
+    }
+
     template = 'contact.html'
     return render(request, template, context)
--- src/seven/settings.py
+++ src/seven/settings.py
@@ -10,8 +10,17 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+
+# Defined in own secrets file (do not source control) in <project>/src/secrets.py
+from secrets import SECRETS
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = SECRETS['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = SECRETS['EMAIL_HOST_PASSWORD']
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
